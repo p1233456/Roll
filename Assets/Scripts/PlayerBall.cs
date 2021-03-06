ab5ad33ad5fcee0c35ab5cb70a2c36b46d3ccfc9
ginyang2 <59835095+ginyang2@users.noreply.github.com>
2021-03-06 10:24:54 +0900

    Add Move

---
+++ Assets/Scripts/PlayerBall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBall : MonoBehaviour
+{
+    private Rigidbody rigid;
+    private float horizontal;
+
+    private void Awake()
+    {
+        rigid = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rigid.AddForce(new Vector3(horizontal, 0, 1), ForceMode.Impulse);
+    }
+}
