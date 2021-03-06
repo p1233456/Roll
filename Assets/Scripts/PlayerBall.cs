d150158118b870019497c6474c563aa1d30fbc97
ginyang2 <59835095+ginyang2@users.noreply.github.com>
2021-03-06 10:37:39 +0900

    Add Jump

--- Assets/Scripts/PlayerBall.cs
+++ Assets/Scripts/PlayerBall.cs
@@ -6,6 +6,7 @@ public class PlayerBall : MonoBehaviour
 {
     private Rigidbody rigid;
     private float horizontal;
+    private bool isJump;
 
     private void Awake()
     {
@@ -27,8 +28,29 @@ public class PlayerBall : MonoBehaviour
         rigid.AddForce(new Vector3(horizontal, 0, 1), ForceMode.Impulse);
     }
 
+    private void Jump()
+    {
+        if (!isJump)
+        {
+            rigid.AddForce(new Vector3(0, 10, 0), ForceMode.Impulse);
+            isJump = true;
+        }
+    }
+
     private void InputKey()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            isJump = false;
+        }
     }
 }
