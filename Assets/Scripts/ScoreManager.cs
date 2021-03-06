0198049b6a72bcf1d86f11a54c09d322697fbfe6
ginyang2 <59835095+ginyang2@users.noreply.github.com>
2021-03-06 11:12:37 +0900

    Add ScoreUIUpdate

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField]
+    private int score;
+    public int Score { get { return score; } }
+
+    [SerializeField]
+    private Text scoreText;
+
+    void Update()
+    {
+        UIUpdate();
+    }
+
+    private void UIUpdate()
+    {
+        scoreText.text = score + "점";
+    }
+}
