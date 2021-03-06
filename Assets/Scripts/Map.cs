59d421a83852adef34fed33b1a3940e2cab0ebcb
ginyang2 <59835095+ginyang2@users.noreply.github.com>
2021-03-06 16:49:48 +0900

    Add MapComponent

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public float MapSize { get { return startPoint.position.z - endPoint.position.z; } }
+    [SerializeField]
+    private Transform startPoint;
+    [SerializeField]
+    private Transform endPoint;
+}
