a7b99b9f70a1e784adf5bfe10e9622d68e8d01b4
ginyang2 <59835095+ginyang2@users.noreply.github.com>
2021-03-06 11:55:34 +0900

    Add WallPrefab

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Wall : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent touchPlayer;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            touchPlayer.Invoke();
+        }
+    }
+}
