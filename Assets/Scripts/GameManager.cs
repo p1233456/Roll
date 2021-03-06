a2b4cd6210aac86ed7cae5b8486980f1b95e1b2c
ginyang2 <59835095+ginyang2@users.noreply.github.com>
2021-03-06 11:47:40 +0900

    Add GameOver, GameStart

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent gameStart;
+    [SerializeField]
+    private UnityEvent gameOver;
+
+    public void GameStart()
+    {
+        gameStart.Invoke();
+    }
+
+    public void GameOver()
+    {
+        gameOver.Invoke();
+    }
+}
